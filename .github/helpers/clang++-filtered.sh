e66d6840268d080e566b62c7a85da6b6cb7fba98
Yancharuk Alexander <alex@itvault.info>
2026-09-22 10:35:07 +0300

    chore: Move clang-wrappers & meson configs into helpers dir

---
+++ .github/helpers/clang++-filtered.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+args=()
+
+for arg in "$@"; do
+	case "$arg" in
+		-Wl,--allow-shlib-undefined|--allow-shlib-undefined) ;;
+		*) args+=("$arg") ;;
+	esac
+done
+
+exec clang++ "${args[@]}"
